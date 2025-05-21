f038dd4edfc9ca4b4cb1fe733e280e3757cef02f
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-05-21 09:25:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
 
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://envs.sh/jUp.jpg")
+    START_PIC   = os.environ.get("START_PIC", "https://ibb.co/ZRf2TD96")
     ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
     # channels logs
